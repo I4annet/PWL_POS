c8e5da32b05e86d0b4671ac229aef60d7602b989
Ivansyah <ivansyahsantoso@gmail.com>
2025-03-04 10:53:43 +0700

    update: menambahkan kolom aksi untuk mengubah atau menghapus data

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,10 +7,21 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+            <td>Aksi</td>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <td>{{$data}}</td>
+            <td>{{$d->user_id}}</td>
+            <td>{{$d->username}}</td>
+            <td>{{$d->nama}}</td>
+            <td>{{$d->level_id}}</td>
+            <td><a href="/user/ubah/{{$d->user_id}}"> Ubah </a> 
+            | <a href="/user/hapus/{{ $d->user_id}}"> Hapus </a></td>
         </tr>
+        @endforeach
 </body>
 </html>
