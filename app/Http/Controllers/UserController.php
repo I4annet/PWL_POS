bd0bc732ec96aa3aee08f98f25f6e8f24cfb28a4
Ivansyah <ivansyahsantoso@gmail.com>
2025-03-04 10:40:46 +0700

    update: memberikan metode isDirty dan isClean

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,30 @@ public function index() {
 
       $user = UserModel::firstOrNew(
         [
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+            'username' => 'manager55',
+            'nama' => 'Manager55',
             'password' => Hash::make('12345'),
-            'level_id' => 2
-        ],
-      );
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager56';
+
+        $user->isDirty();
+        $user->isDirty('username');
+        $user->isDirty('nama');
+        $user->isDirty(['nama', 'username']);
+
+        $user->isClean();
+        $user->isClean('username');
+        $user->isClean('nama');
+        $user->isClean(['nama', 'username']);
+
+        $user->save();
+
+        $user->isDirty();
+        $user->isClean();
+
+        dd($user->isDirty());
 
       return view('user', ['data' => $user]);
     }
