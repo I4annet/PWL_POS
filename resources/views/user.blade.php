8e7f11b97bca0ffb5b86491a5a47ec48038a644b
Ivansyah <ivansyahsantoso@gmail.com>
2025-03-05 17:27:37 +0700

    update: membuat form create atau tambah

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,24 +4,28 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
-            <td>Aksi</td>
-        </tr>
-        @foreach ($data as $d)
-        <tr>
-            <td>{{$d->user_id}}</td>
-            <td>{{$d->username}}</td>
-            <td>{{$d->nama}}</td>
-            <td>{{$d->level_id}}</td>
-            <td><a href="/user/ubah/{{$d->user_id}}"> Ubah </a> 
-            | <a href="/user/hapus/{{ $d->user_id}}"> Hapus </a></td>
-        </tr>
-        @endforeach
+    <h1>Form Tambah Data User</h1>
+        <form method="post" action="/user/tambah_simpan">
+        {{ csrf_field()}}
+
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Masukkan Username">
+        <br>
+
+        <label>Nama</label>
+        <input type="text" name="name" placeholder="Masukkan Nama">
+        <br>
+
+        <label>Password</label>
+        <input type="text" name="password" placeholder="Masukkan Password">
+        <br>
+
+        <label>Level ID</label>
+        <input type="number" name="level_id" placeholder="Masukkan ID Level">
+        <br><br>
+
+        <input type="submit" class="btn btn-success" value="Simpan">
+
+        </form>
 </body>
 </html>
