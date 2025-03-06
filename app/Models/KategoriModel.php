534fb0e0359aed786ea5ee685835e66af765f999
Ivansyah <ivansyahsantoso@gmail.com>
2025-03-06 11:35:07 +0700

    add: tambah file KategoriModel.php

---
+++ app/Models/KategoriModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriModel extends Model
+{
+    public function barang() {
+        return $this->hasMany(BarangModel::class, 'barang_id', 'barang_id');
+    }
+}
