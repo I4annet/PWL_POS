ea3dcf6937c7b3ea531b631fd2eb1217a416851d
Ivansyah <ivansyahsantoso@gmail.com>
2025-03-18 09:34:11 +0700

    update: menambahkan route create_ajax

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ Route::get('/', [WelcomeController::class, 'index']);
 Route::group(['prefix' => 'user'], function () {
     Route::get('/', [UserController::class, 'index']);          // Menampilkan halaman awal user
     Route::post('/list', [UserController::class, 'list']);      // Menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']);  // Menampilkan halaman form tambah user Ajax
+    Route::post('/ajax', [UserController::class, 'store_ajax']);  // Menyimpan data user baru Ajax
     Route::get('/create', [UserController::class, 'create']);   // Menampilkan halamn form tambah user
     Route::post('/', [UserController::class, 'store']);         // Menyimpan data user baru  
     Route::get('/{id}', [UserController::class, 'show']);       // Menampilkan detail user
