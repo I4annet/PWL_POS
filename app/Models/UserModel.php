9cfde4ab0f5d19048e679d589d7acb4816730157
Ivansyah <ivansyahsantoso@gmail.com>
2025-03-23 18:39:54 +0700

    edit: menambahkan function getRole dan hasRole

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -30,4 +30,12 @@ class UserModel extends Model
     public function level() {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    public function getRoleName() {
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role) {
+        return $this->level->level_kode == $role;
+    }   
 }
