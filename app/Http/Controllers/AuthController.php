67576d71644141aa51d78814a08eeb0b90ada9fc
Ivansyah <ivansyahsantoso@gmail.com>
2025-03-26 18:43:02 +0700

    Jobsheet 7 Tugas 4 Form Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+// use Dotenv\Validator;
+
+use App\Models\LevelModel;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use app\Models\UserModel;
 
 class AuthController extends Controller
 {
@@ -41,4 +46,44 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
         return redirect('login');
     }
+
+    public function register() {
+        $levels = LevelModel::all();
+
+        return view ('auth.register', compact('levels'));
+    }
+
+    public function store(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'username' => 'required|string|min:4|unique:m_user,username',
+        'nama' => 'required|string|max:100',
+        'password' => 'required|min:5',
+        'level_id' => 'required|exists:m_level,level_id',
+        
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'status' => false,
+            'message' => 'Validasi Gagal!',
+            'msgField' => $validator->errors()
+        ], 422);
+    }
+
+    $user = UserModel::create([
+        'username' => $request->username,
+        'nama' => $request->nama,
+        'password' => bcrypt($request->password), 
+        'level_id' => $request->level_id,
+    ]);
+
+    Auth::login($user);
+
+    return response()->json([
+        'status' => true,
+        'message' => 'Registrasi Berhasil!',
+        'redirect' => url('/')
+    ], 200);
+}
 }
