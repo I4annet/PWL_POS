48cdd89e101eca62745ddd5962c89934b800c2aa
Ivansyah <ivansyahsantoso@gmail.com>
2025-04-20 17:18:27 +0700

    revisi kode migration dan menambahkan migration tabel Stok Barang

--- database/migrations/2025_02_25_014920_m_kategori_table.php
+++ database/migrations/2025_02_25_014920_m_kategori_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10)->unique(); 
+            $table->string('kategori_nama', 255);
+            $table->timestamps(); 
         });
     }
 
--- database/migrations/2025_02_25_015018_m_supplier_table.php
+++ database/migrations/2025_02_25_015018_m_supplier_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_supplier', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('supplier_id');  // Mengubah id menjadi supplier_id
+            $table->string('supplier_kode', 10)->unique();
+            $table->string('nama_supplier', 100);
             $table->timestamps();
         });
     }
